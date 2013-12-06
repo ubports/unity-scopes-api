4a5c3718b120148dccc012b38a801bb3519dfa95
James Henstridge <james@jamesh.id.au>
2013-12-06 11:18:35 +0000

    Move Runtime::run_scope() implementation to RuntimeImpl.

--- src/Runtime.cpp
+++ src/Runtime.cpp
@@ -16,10 +16,7 @@
  * Authored by: Michi Henning <michi.henning@canonical.com>
  */
 
-#include <future>
-
 #include <scopes/Runtime.h>
-#include <scopes/ScopeBase.h>
 
 #include <scopes/internal/RuntimeImpl.h>
 
@@ -70,24 +67,7 @@ RegistryProxy Runtime::registry() const
 
 void Runtime::run_scope(ScopeBase *const scope_base)
 {
-    auto mw = p->factory()->create(p->scope_name(), "Zmq", "Zmq.ini");
-
-    scope_base->start(p->scope_name(), p->registry());
-    // Ensure the scope gets stopped.
-    unique_ptr<ScopeBase, void(*)(ScopeBase*)> cleanup_scope(scope_base, [](ScopeBase *scope_base) { scope_base->stop(); });
-
-    // Give a thread to the scope to do with as it likes. If the scope
-    // doesn't want to use it and immediately returns from run(),
-    // that's fine.
-    auto run_future = std::async(launch::async, [scope_base] { scope_base->run(); });
-
-    // Create a servant for the scope and register the servant.
-    auto scope = unique_ptr<internal::ScopeObject>(new internal::ScopeObject(p.get(), scope_base));
-    auto proxy = mw->add_scope_object(p->scope_name(), move(scope));
-
-    mw->wait_for_shutdown();
-
-    run_future.get();
+    p->run_scope(scope_base);
 }
 
 } // namespace scopes
