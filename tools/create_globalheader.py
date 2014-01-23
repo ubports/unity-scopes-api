609d1f37d1c2d06a133e389ee2699aa6266b9638
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2014-01-23 14:05:16 +0200

    Started work on unified header generator script.

---
+++ tools/create_globalheader.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2014 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authored by: Jussi Pakkanen <jussi.pakkanen@canonical.com>
+
+
+from glob import glob
+import sys, os
+
+def build_header(inc_root, outfile):
+    for f in glob(os.path.join(inc_root, 'unity/scopes', '*.h')):
+        base = os.path.split(f)[1]
+        line = '#include<unity/scopes/%s>\n' % base
+        print(line)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print(sys.argv[0], 'include_root outfile')
+        sys.exit(1)
+    incroot = sys.argv[1]
+    outfile = sys.argv[2]
+    build_header(incroot, outfile)
