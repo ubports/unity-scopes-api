2c6b9a93798422a4117e37fd5c850d83c1ad22a9
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2014-02-12 12:21:41 +0200

    Conigured upstart jobs to start smart-scopes-proxy first then scope-registry once the proxy is ready

--- smartscopesproxy/smartscopesproxy.cpp
+++ smartscopesproxy/smartscopesproxy.cpp
@@ -50,16 +50,27 @@ int main(int argc, char* argv[])
 
     int exit_status = 1;
 
-    // argv[1]: server_url_env
+    bool sig_upstart = false;
     std::string server_url_env;
-    if (argc > 1)
-    {
-        server_url_env = "SMART_SCOPES_SERVER=" + std::string(argv[1]);
-        ::putenv(const_cast<char*>(server_url_env.c_str()));
-    }
+    std::string config_file;
 
-    // argv[2]: config_file
-    std::string config_file = argc > 2 ? argv[2] : "";
+    // check for "upstart" as first arg
+    if (argc > 1 && std::string(argv[1]) == "upstart")
+    {
+        sig_upstart = true;
+    }
+    else
+    {
+        // argv[1]: server_url_env
+        if (argc > 1)
+        {
+            server_url_env = "SMART_SCOPES_SERVER=" + std::string(argv[1]);
+            ::putenv(const_cast<char*>(server_url_env.c_str()));
+        }
+
+        // argv[2]: config_file
+        config_file = argc > 2 ? argv[2] : "";
+    }
 
     try
     {
@@ -93,6 +104,12 @@ int main(int argc, char* argv[])
         // Add objects to the middlewares
         reg_mw->add_registry_object(reg_rt->registry_identity(), reg);
         scope_mw->add_dflt_scope_object(std::move(scope));
+
+        if (sig_upstart)
+        {
+            // signal to upstart that we are ready
+            kill(getpid(), SIGSTOP);
+        }
 
         // Wait until shutdown
         scope_mw->wait_for_shutdown();
