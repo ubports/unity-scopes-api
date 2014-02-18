2bd4680533991f878521b1ec52dfb144eb761917
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2014-02-18 12:20:28 +0200

    Added EXPECT_TRUE for get_remote_scopes() in SmartScopesClient_test

--- test/gtest/scopes/internal/smartscopes/SmartScopesClient/SmartScopesClient_test.cpp
+++ test/gtest/scopes/internal/smartscopes/SmartScopesClient/SmartScopesClient_test.cpp
@@ -58,8 +58,8 @@ protected:
 TEST_F(SmartScopesClientTest, remote_scopes)
 {
     std::vector<RemoteScope> scopes;
-    ssc_->get_remote_scopes(scopes, "", false);
 
+    EXPECT_TRUE(ssc_->get_remote_scopes(scopes, "", false));
     ASSERT_EQ(2, scopes.size());
 
     EXPECT_EQ("dummy.scope", scopes[0].id);
