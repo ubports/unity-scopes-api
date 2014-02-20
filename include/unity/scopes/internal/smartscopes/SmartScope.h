091820d939b582c04b4336e2c2b08252f4b935b5
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2014-02-20 10:18:03 +0200

    Removed limit from "TODO:"

--- include/unity/scopes/internal/smartscopes/SmartScope.h
+++ include/unity/scopes/internal/smartscopes/SmartScope.h
@@ -53,7 +53,7 @@ public:
         SmartScopesClient::SPtr ss_client = reg->get_ssclient();
         std::string base_url = reg->get_base_url(scope_id_);
 
-        ///! TODO: session_id, query_id, locale, country, limit
+        ///! TODO: session_id, query_id, locale, country
         search_handle_ = ss_client->search(base_url, query_.query_string(), "session_id", 0, hints.form_factor(), "", "", hints.cardinality());
     }
 
