2ffbdc2ee762f05c6c5705e6eed613ef7227630d
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2014-02-25 12:02:25 +0200

    Removed unnessesary pimpl structure from RegistryObject as it is an internal namespace class.

--- include/unity/scopes/internal/RegistryObject.h
+++ include/unity/scopes/internal/RegistryObject.h
@@ -57,8 +57,17 @@ public:
     void set_remote_registry(MWRegistryProxy const& remote_registry);
 
 private:
-    RegistryObjectPrivate* p;
+    void spawn_scope(std::string const& scope_name);
+    int kill_process(pid_t pid);
+    void shutdown();
+    static bool is_dead(pid_t pid);
+
+private:
     mutable std::mutex mutex_;
+    MetadataMap scopes_;
+    std::map<std::string, pid_t> scope_processes_;
+    std::map<std::string, std::vector<std::string>> commands_;
+    MWRegistryProxy remote_registry_;
 };
 
 } // namespace internal
