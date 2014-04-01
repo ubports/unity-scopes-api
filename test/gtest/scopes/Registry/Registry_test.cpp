f3e553487af384c428dcb383721feb80d2f31591
Pawel Stolowski <pawel.stolowski@canonical.com>
2014-04-01 18:58:32 +0200

    First cut registry test.

---
+++ test/gtest/scopes/Registry/Registry_test.cpp
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2013 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Pawel Stolowski <pawel.stolowski@canonical.com>
+ */
+
+#include <unity/scopes/Runtime.h>
+#include <unity/scopes/Registry.h>
+#include <unity/scopes/SearchMetadata.h>
+#include <unity/scopes/SearchListenerBase.h>
+#include <unity/scopes/CategorisedResult.h>
+#include <functional>
+#include <gtest/gtest.h>
+#include <signal.h>
+#include <unistd.h>
+
+using namespace unity::scopes;
+
+class Receiver : public SearchListenerBase
+{
+public:
+    virtual void push(CategorisedResult /* result */) override
+    {
+    }
+
+    virtual void finished(ListenerBase::Reason /* reason */, std::string const& /* error_message */) override
+    {
+    }
+};
+
+TEST(Registry, metadata)
+{
+    Runtime::UPtr rt = Runtime::create(TEST_RUNTIME_FILE);
+    RegistryProxy r = rt->registry();
+
+    auto meta = r->get_metadata("testscopeA");
+    EXPECT_EQ("testscopeA", meta.scope_id());
+    EXPECT_EQ("Canonical Ltd.", meta.author());
+    EXPECT_EQ("scope-A.DisplayName", meta.display_name());
+    EXPECT_EQ("scope-A.Description", meta.description());
+    EXPECT_EQ("/scope-A.Art", meta.art());
+    EXPECT_EQ("/scope-A.Icon", meta.icon());
+    EXPECT_EQ("scope-A.HotKey", meta.hot_key());
+    EXPECT_EQ("scope-A.SearchHint", meta.search_hint());
+
+    const char *bart = TEST_RUNTIME_PATH "/scopes/testscopeB/scope-B.Art";
+    const char *bicon = TEST_RUNTIME_PATH "/scopes/testscopeB/scope-B.Icon";
+
+    meta = r->get_metadata("testscopeB");
+    EXPECT_EQ("testscopeB", meta.scope_id());
+    EXPECT_EQ("Canonical Ltd.", meta.author());
+    EXPECT_EQ("scope-B.DisplayName", meta.display_name());
+    EXPECT_EQ("scope-B.Description", meta.description());
+    EXPECT_EQ(bart, meta.art());
+    EXPECT_EQ(bicon, meta.icon());
+    EXPECT_EQ("scope-B.HotKey", meta.hot_key());
+    EXPECT_EQ("scope-B.SearchHint", meta.search_hint());
+
+    auto sp = meta.proxy();
+
+    SearchListenerBase::SPtr reply(new Receiver);
+    SearchMetadata metadata("C", "desktop");
+
+    // search would fail if testscopeB can't be executed
+    try
+    {
+        auto ctrl = sp->search("foo", metadata, reply);
+    }
+    catch (...)
+    {
+        FAIL();
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    auto rpid = fork();
+    if (rpid == 0)
+    {
+        char *args[3];
+        args[0] = "scoperegistry [Registry test]";
+        args[1] = TEST_RUNTIME_FILE;
+        args[2] =  nullptr;
+        if (execv(TEST_RUNTIME_PATH "/../../../../scoperegistry/scoperegistry", args) < 0)
+        {
+            perror("Error starting scoperegistry:");
+        }
+        return 1;
+    }
+    else if (rpid > 0)
+    {
+        auto rc = RUN_ALL_TESTS();
+        kill(rpid, SIGSTOP);
+        return rc;
+    }
+    else
+    {
+        perror("Failed to fork:");
+    }
+    return 1;
+}
