6992d348e8ba92fc10ca4c80ff7e8868d6ccbe17
James Henstridge <james@jamesh.id.au>
2014-04-02 10:44:15 +0100

    Add public methods for serialization and deserialization of variants to/from JSON encoded strings.

--- include/unity/scopes/Variant.h
+++ include/unity/scopes/Variant.h
@@ -180,6 +180,17 @@ public:
     */
     void swap(Variant& other) noexcept;
 
+    /**@name Serialisation
+    */
+    /**
+    \brief serializes the variant to a JSON encoded string.
+    */
+    std::string serialize_json() const;
+    /**
+    \brief deserializes a JSON encoded string to a Variant.
+    */
+    static Variant deserialize_json(std::string const& json_string);
+
 private:
     Variant(internal::NullVariant const&);
 
