ac642fac0041b03ade162de154bb7ccf5bbb8312
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2014-04-09 17:25:22 +0100

    Init state receiver first

--- scoperegistry/scoperegistry.cpp
+++ scoperegistry/scoperegistry.cpp
@@ -444,13 +444,13 @@ main(int argc, char* argv[])
             load_remote_scopes(registry, middleware, ss_reg_id, ss_reg_endpoint);
         }
 
+        // Let's add the registry's state receiver to the middleware so that scopes can inform
+        // the registry of state changes.
+        middleware->add_state_receiver_object("StateReceiver", registry->state_receiver());
+
         // Now that the registry table is populated, we can add the registry to the middleware, so
         // it starts processing incoming requests.
         middleware->add_registry_object(runtime->registry_identity(), registry);
-
-        // We also add the registry's state receiver to the middleware so that scopes can inform
-        // the registry of state changes.
-        middleware->add_state_receiver_object("StateReceiver", registry->state_receiver());
 
         // FIXME, HACK HACK HACK HACK
         // The middleware should spawn scope processes with lookup() on demand.
