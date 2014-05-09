74226b2529ef7f18ac48ac93cc35326848063518
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2014-05-09 08:53:14 +0200

    Cleaned up idle_timeout_ initialisation after new config updates

--- src/scopes/internal/ScopeConfig.cpp
+++ src/scopes/internal/ScopeConfig.cpp
@@ -124,16 +124,8 @@ ScopeConfig::ScopeConfig(string const& configfile) :
     {
     }
 
-    ///! TODO: replace code below with commented-out line once config-fixes-stage2 lands
-    // idle_timeout_ = get_optional_int(scope_config_group, idle_timeout_key, DFLT_SCOPE_IDLE_TIMEOUT);
-    try
-    {
-        idle_timeout_ = parser()->get_int(scope_config_group, idle_timeout_key);
-    }
-    catch (unity::LogicException const&)
-    {
-        idle_timeout_ = DFLT_SCOPE_IDLE_TIMEOUT;
-    }
+    idle_timeout_ = get_optional_int(scope_config_group, idle_timeout_key, DFLT_SCOPE_IDLE_TIMEOUT);
+
     // Negative values and values greater than max int (once multiplied by 1000 (s to ms)) are illegal
     const int max_idle_timeout = std::numeric_limits<int>::max() / 1000;
     if (idle_timeout_ < 0 || idle_timeout_ > max_idle_timeout)
