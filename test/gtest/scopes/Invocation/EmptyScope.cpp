e87bfef03e2430094e8c1524ca6e4aa2596251b8
Michi Henning <michi.henning@canonical.com>
2014-05-13 11:40:09 +1000

    Fixed bug that caused a crash if the client shut down the run time without waiting for outstanding async invocations to complete.

---
+++ test/gtest/scopes/Invocation/EmptyScope.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2014 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Michi Henning <michi.henning@canonical.com>
+ */
+
+#include "EmptyScope.h"
+
+#include <unity/scopes/CategorisedResult.h>
+#include <unity/scopes/ScopeBase.h>
+#include <unity/scopes/SearchReply.h>
+
+#include <mutex>
+#include <thread>
+
+using namespace std;
+using namespace unity::scopes;
+
+namespace
+{
+
+class TestQuery : public SearchQueryBase
+{
+public:
+    TestQuery()
+    {
+    }
+
+    virtual void cancelled() override
+    {
+    }
+
+    virtual void run(SearchReplyProxy const& /* reply */) override
+    {
+    }
+};
+
+}  // namespace
+
+int EmptyScope::start(string const&, RegistryProxy const &)
+{
+    return VERSION;
+}
+
+void EmptyScope::stop()
+{
+}
+
+void EmptyScope::run()
+{
+}
+
+SearchQueryBase::UPtr EmptyScope::search(CannedQuery const&, SearchMetadata const &)
+{
+    return SearchQueryBase::UPtr(new TestQuery());
+}
+
+PreviewQueryBase::UPtr EmptyScope::preview(Result const&, ActionMetadata const &)
+{
+    return nullptr;  // unused
+}
