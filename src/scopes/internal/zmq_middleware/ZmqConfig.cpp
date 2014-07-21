30de8382b1475a35a07e01dbaa7336a93fc689a2
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2014-07-21 08:34:24 +0200

    Give the registry more time to locate a scope

--- src/scopes/internal/zmq_middleware/ZmqConfig.cpp
+++ src/scopes/internal/zmq_middleware/ZmqConfig.cpp
@@ -80,7 +80,7 @@ ZmqConfig::ZmqConfig(string const& configfile) :
     }
 
     locate_timeout_ = get_optional_int(zmq_config_group, locate_timeout_key, DFLT_ZMQ_LOCATE_TIMEOUT);
-    if (locate_timeout_ < 10 || locate_timeout_ > 5000)
+    if (locate_timeout_ < 10 || locate_timeout_ > 15000)
     {
         throw_ex("Illegal value (" + to_string(locate_timeout_) + ") for " + locate_timeout_key + ": value must be 10-5000");
     }
