ee00316e3b51f7fe6aee24a40cec1c785db4ba7e
Pete Woods <pete.woods@canonical.com>
2014-07-25 16:09:20 +0100

    Confess to writing these classes

--- src/scopes/internal/LocationImpl.cpp
+++ src/scopes/internal/LocationImpl.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Canonical Ltd
+ * Copyright (C) 2014 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the Lesser GNU General Public License version 3 as
@@ -13,7 +13,7 @@
  * You should have received a copy of the Lesser GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Authored by: Michi Henning <michi.henning@canonical.com>
+ * Authored by: Pete Woods <pete.woods@canonical.com>
  */
 
 #include <unity/scopes/internal/LocationImpl.h>
--- test/gtest/scopes/Location/Location_test.cpp
+++ test/gtest/scopes/Location/Location_test.cpp
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Authored by: Pawel Stolowski <pawel.stolowski@canonical.com>
+ * Authored by: Pete Woods <pete.woods@canonical.com>
 */
 
 #include <unity/scopes/ScopeExceptions.h>
