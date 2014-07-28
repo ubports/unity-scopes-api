ff7d1202d023cf08ade005c3955ffbee5878303b
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2014-07-28 07:44:09 +0200

    Started fleshing out CompletionDetails

--- include/unity/scopes/CompletionDetails.h
+++ include/unity/scopes/CompletionDetails.h
@@ -19,8 +19,10 @@
 #ifndef UNITY_SCOPES_COMPLETIONDETAILS_H
 #define UNITY_SCOPES_COMPLETIONDETAILS_H
 
+#include <unity/scopes/OperationInfo.h>
 #include <unity/util/NonCopyable.h>
 
+#include <list>
 #include <memory>
 #include <string>
 
@@ -48,9 +50,48 @@ public:
     NONCOPYABLE(CompletionDetails);
     /// @endcond
 
+    /**
+    \brief Indicates the completion status for a query.
+
+    The `Error` enumerator indicates that a query terminated abnormally, for example, a scope could
+    not be reached over the network or explicitly reported an error.
+    */
+    enum CompletionStatus
+    {
+      OK,
+      Cancelled,
+      Error
+    };
+
+    /**
+    \brief Create CompletionDetails with the given completion status code.
+    \param status Indicates the completion status of the query.
+    */
+    CompletionDetails(CompletionStatus status);
+
+    /**
+    \brief Create CompletionDetails with the given completion status code and details about the
+    query operation.
+    \param status Indicates the completion status of the query.
+    \param details Contains additional information regarding the query operation.
+    */
+    CompletionDetails(CompletionStatus status, std::list<OperationInfo> const& details);
+
     /// @cond
     ~CompletionDetails();
     /// @endcond
+
+    /**
+    \brief Get the completion status code.
+    \return Enum indicating the completion status of the query.
+    */
+    CompletionStatus status() const noexcept;
+
+    /**
+    \brief Get more details about the query operation.
+    \return List containing additional information regarding the query operation.
+    */
+    std::list<OperationInfo> details() const noexcept;
 
 private:
     std::unique_ptr<internal::CompletionDetailsImpl> p;
