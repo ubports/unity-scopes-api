493a8cd2463cee52644e8777b32d391332b7d728
Pawel Stolowski <pawel.stolowski@canonical.com>
2015-02-02 10:14:18 +0100

    Copyright and a comment.

--- data/libunity-scopes.py
+++ data/libunity-scopes.py
@@ -1,3 +1,27 @@
+#
+# Copyright (C) 2015 Canonical Ltd.
+# Author: Pawel Stolowski <pawel.stolowski@canonical.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+#
+# This script parses ProcCmdline to check if this is a crash of scoperunner process.
+# If so, it checks what scope made it crash and adjusts Package and SourcePackage
+# attributes of the crash report to point at the crashing scope.
+#
+
 from os import path
 import subprocess, json, configparser
 
