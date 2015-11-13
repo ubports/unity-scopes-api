eb2d00206141ca6086fd9ed0044b2a94cd6fd2c4
Michi Henning <michi.henning@canonical.com>
2015-11-13 14:19:28 +1000

    Fixed stale comments.

--- test/copyright/check_copyright.sh
+++ test/copyright/check_copyright.sh
@@ -19,9 +19,7 @@
 #
 
 #
-# Check that, somewhere in the first 30 lines of each file, the string "Copyright" (case independent) appears.
-# Print out a messsage for each file without a copyright notice and exit with non-zero status
-# if any such file is found.
+# Check that we have acceptable license information in our source files.
 #
 
 usage()
@@ -37,7 +35,7 @@ ignore_pat="\\.sci$"
 
 #
 # We don't use the -i option of licensecheck to add ignore_dir to the pattern because Jenkins creates directories
-# with names that contain regex meta-characters, such as "." and "+". Instead, if ingnore_dir is set, we post-filter
+# with names that contain regex meta-characters, such as "." and "+". Instead, if ignore_dir is set, we post-filter
 # the output with grep -F, so we don't get false positives from licensecheck.
 #
 
