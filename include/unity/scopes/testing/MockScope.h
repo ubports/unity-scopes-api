cbf1059ff0ee0f8d6b48428097c0bb4de98aca10
Pawel Stolowski <pawel.stolowski@canonical.com>
2016-08-18 13:20:17 +0200

    Added missing includes as apparently g++ is more picky now

--- include/unity/scopes/testing/MockScope.h
+++ include/unity/scopes/testing/MockScope.h
@@ -19,6 +19,8 @@
 #pragma once
 
 #include <unity/scopes/Scope.h>
+#include <unity/scopes/SearchMetadata.h>
+#include <unity/scopes/ActionMetadata.h>
 
 #include <unity/scopes/testing/MockObject.h>
 
